c991b2279d95c5d9f21e8e9b59e7451139e5abf2
Pietro Granda Boscarato <boscaratopietro@gmail.com>
2025-11-29 22:28:38 +0100

    Add C program that prints 'Ciao, mondo!'

---
+++ C/Ciao_mondo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>      // Include la libreria standard per l'input/output
+
+int main() {            // Funzione principale del programma
+    printf("Ciao, mondo!\n");   // Stampa un messaggio sullo schermo
+    return 0;           // Termina il programma
+}

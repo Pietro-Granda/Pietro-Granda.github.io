3aab16490b5be53f3888af8e343c54aa1cdc74fc
Pietro Granda Boscarato <boscaratopietro@gmail.com>
2025-12-03 22:32:15 +0100

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,18 +1,6 @@
-const toggleBtn = document.getElementById('theme-toggle');
-const body = document.body;
+const themeToggle = document.getElementById('theme-toggle');
 
-// Aplica o tema salvo no localStorage
-if (localStorage.getItem('theme') === 'dark') {
-    body.setAttribute('data-theme', 'dark');
-}
-
-// Alterna tema claro/escuro
-toggleBtn.addEventListener('click', () => {
-    if (body.getAttribute('data-theme') === 'dark') {
-        body.removeAttribute('data-theme');
-        localStorage.setItem('theme', 'light');
-    } else {
-        body.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
 });
+

a93aa10af7b52c5260de2e1e95672b2a5178ae84
Pietro Granda Boscarato <boscaratopietro@gmail.com>
2025-12-14 20:48:48 +0100

    Create New_line.c

---
+++ C/New_line.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("I am learning C.");
+
+  printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+
+  printf("Hello World!\n\n");
+  printf("I am learning C.");
+  return 0;
+}
